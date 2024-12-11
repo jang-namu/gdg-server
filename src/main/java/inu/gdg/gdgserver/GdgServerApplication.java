1ac05d56de352ecd5a95223540112da560acc971
jang-namu <ironhee8005@gmail.com>
2024-12-11 14:38:22 +0900

    init: project

---
+++ src/main/java/inu/gdg/gdgserver/GdgServerApplication.java
@@ -0,0 +1,13 @@
+package inu.gdg.gdgserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GdgServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GdgServerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/inu/gdg/gdgserver/GdgServerApplicationTests.java
@@ -0,0 +1,13 @@
+package inu.gdg.gdgserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GdgServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
